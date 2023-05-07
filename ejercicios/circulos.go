9a5e45623318d3c48ac57c0a2a8b0e5cedab549a
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-05-07 18:33:19 -0400

    Se corrige nombre constante

--- ejercicios/circulos.go
+++ ejercicios/circulos.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	var radio float64
-	const PI = 3.14159265359
+	const Pi = 3.14159265359
 
 	fmt.Println("Ingrese el radio del circulo: ")
 	fmt.Scanln(&radio)
-	perimetro := (radio * 2) * PI
-	area := PI*(math.Pow(radio, 2))
+	perimetro := (radio * 2) * Pi
+	area := Pi*(math.Pow(radio, 2))
 	fmt.Printf("El perimetro del circulo es: %.1f \n", perimetro)
 	fmt.Printf("El area del circulo es: %.1f", area)
 
